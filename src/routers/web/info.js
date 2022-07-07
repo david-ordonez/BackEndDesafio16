7f9fcabf8b69c2e4940745b21ede91cbfa1c793d
David Ordoñez <david.ordonez@globant.com>
2022-07-07 00:20:29 -0500

    [Test] pruebas con prof

--- src/routers/web/info.js
+++ src/routers/web/info.js
@@ -2,32 +2,26 @@ import { Router } from 'express'
 import os  from 'os';
 import compression from 'compression';
 
-const infoWebRouter = new Router()
+const infoWebRouter = new Router();
+const infoObject = {
+    "Argumentos de entrada": process.argv,
+    "Sistema Operativo": process.platform,
+    "Version de node" : process.version,
+    "Memoria total reservada": process.memoryUsage().rss,
+    "Path de ejecucion" : process.execPath,
+    "Process id" : process.pid,
+    "Carpeta del proyecto": process.cwd(),
+    "Numero Procesadores": os.cpus().length
+   };
+
 
 infoWebRouter.get('/info', compression(), (req, res) => {
-    res.json({
-        "Argumentos de entrada": process.argv,
-        "Sistema Operativo": process.platform,
-        "Version de node" : process.version,
-        "Memoria total reservada": process.memoryUsage().rss,
-        "Path de ejecucion" : process.execPath,
-        "Process id" : process.pid,
-        "Carpeta del proyecto": process.cwd(),
-        "Numero Procesadores": os.cpus().length
-       });
+    console.log(JSON.stringify(infoObject));
+    res.json(infoObject);
 })
 
 infoWebRouter.get('/infoSin', (req, res) => {
-    res.json({
-        "Argumentos de entrada": process.argv,
-        "Sistema Operativo": process.platform,
-        "Version de node" : process.version,
-        "Memoria total reservada": process.memoryUsage().rss,
-        "Path de ejecucion" : process.execPath,
-        "Process id" : process.pid,
-        "Carpeta del proyecto": process.cwd(),
-        "Numero Procesadores": os.cpus().length
-       });
+    res.json(infoObject);
 })
 
 export default infoWebRouter
